64b069a2517218f5de50378ec974662d5f0b14ca
washcoat00 <washcoat00@gmail.com>
2026-03-12 23:54:40 +0000

    Initialize project and add project files

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -5,8 +5,9 @@
   channel = "stable-23.11"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    pkgs.nodejs_20
-    pkgs.python3
+    (pkgs.python311.withPackages (ps: with ps; [
+      pip
+    ]))
   ];
   # Sets environment variables in the workspace
   env = {};
@@ -19,27 +20,7 @@
     # Enable previews and customize configuration
     previews = {
       enable = true;
-      previews = {
-        web = {
-          command = ["python3" "-m" "http.server" "$PORT" "--bind" "0.0.0.0"];
-          manager = "web";
-        };
-      };
-    };
-    # Workspace lifecycle hooks
-    workspace = {
-      # Runs when a workspace is first created
-      onCreate = {
-        # Example: install JS dependencies from NPM
-        # npm-install = "npm install";
-        # Open editors for the following files by default, if they exist:
-        default.openFiles = [ "style.css" "main.js" "index.html" ];
-      };
-      # Runs when the workspace is (re)started
-      onStart = {
-        # Example: start a background task to watch and re-build backend code
-        # watch-backend = "npm run watch-backend";
-      };
+      previews = [];
     };
   };
 }
